22c6b8661641aea3de77334cafd62c5500982a9f
SMS-79 <salvamtnez79@gmail.com>
2026-03-10 20:38:59 +0100

    Commit inicial

---
+++ src/main/java/inf/pds/proy/PdsProyectoApplication.java
@@ -0,0 +1,13 @@
+package inf.pds.proy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdsProyectoApplication.class, args);
+	}
+
+}
