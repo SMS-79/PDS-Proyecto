0d6ecc53332073d49d8576e00fcdfc1eb99a7a68
PEYLAU <pedrochicocaballero@gmail.com>
2026-05-28 11:11:47 +0200

    Subida de todos los archivos

--- PDSProyecto/src/main/java/inf/pds/proy/domain/model/TarjetaTarea.java
+++ PDSProyecto/src/main/java/inf/pds/proy/domain/model/TarjetaTarea.java
@@ -1,11 +1,47 @@
 package inf.pds.proy.domain.model;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public class TarjetaTarea extends Tarjeta {
+	
+	private String descripcion; 
+	private LocalDate fechaLimite; 
+	private Usuario reponsable; 
 	
 	public TarjetaTarea(UUID id, String nombre, Etiqueta etiqueta) {
 		super(id, nombre, etiqueta);
 	}
 
+    public TarjetaTarea(String nombre, Etiqueta etiqueta, String descripcion, LocalDate fechaLimite) {
+        super(UUID.randomUUID(), nombre, etiqueta);
+        this.descripcion = descripcion;
+        this.fechaLimite = fechaLimite;
+    }
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LocalDate getFechaLimite() {
+		return fechaLimite;
+	}
+
+	public void setFechaLimite(LocalDate fechaLimite) {
+		this.fechaLimite = fechaLimite;
+	}
+
+	public Usuario getReponsable() {
+		return reponsable;
+	}
+
+	public void setReponsable(Usuario reponsable) {
+		this.reponsable = reponsable;
+	}
+    
+    
 }
