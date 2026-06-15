b2ca3939b75ae2ff83b7b73ddf2adab1e5030405
PEYLAU <pedrochicocaballero@gmail.com>
2026-06-15 20:55:03 +0200

    Configuración báscica de la interfaz javaFX

--- PDSProyecto/src/main/java/inf/pds/proy/PdsProyectoApplication.java
+++ PDSProyecto/src/main/java/inf/pds/proy/PdsProyectoApplication.java
@@ -1,13 +1,15 @@
 package inf.pds.proy;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import inf.pds.proy.adapters.ui.AplicacionJavaFx;
+import javafx.application.Application;
 
 @SpringBootApplication
 public class PdsProyectoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PdsProyectoApplication.class, args);
+		
+		Application.launch(AplicacionJavaFx.class, args);
 	}
 
 }
